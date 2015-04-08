3de4a7c2aa6ff27e40e92c918ffe9c5450d8677f
Alexander von Below <Alex@vonBelow.Com>
2015-04-08 22:51:34 +0200

    Getting path to currently selected xcode

--- package_ipa.sh
+++ package_ipa.sh
@@ -3,6 +3,12 @@ APP="$1"
 IPA="$2"
 TEMP_IPA_BUILT="/tmp/ipabuild"
 
+DEVELOPER_DIR=`xcode-select --print-path`
+if [ ! -d "${DEVELOPER_DIR}" ]; then
+	echo "No developer directory found!"
+	exit 1
+fi
+
 if [ ! -d "${APP}" ]; then
     echo "Usage: sh package_ipa.sh PATH_TO_SIGNED_APP OUTPUT_IPA_PATH"
     exit 1
@@ -30,7 +36,7 @@ then
 
     for SWIFT_LIB in $(ls -1 ${APP}/Frameworks); do 
         echo "Copying ${SWIFT_LIB}"
-        cp "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/iphoneos/${SWIFT_LIB}" "${TEMP_IPA_BUILT}/SwiftSupport"
+        cp "${DEVELOPER_DIR}/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/iphoneos/${SWIFT_LIB}" "${TEMP_IPA_BUILT}/SwiftSupport"
     done
 fi
 
